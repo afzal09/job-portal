4792beec93a09d18c34d9a79f8d1d202740720e7
afzal <afzalmomin@gmail.com>
2024-12-30 16:02:29 +0530

    authentication system working using middlewares

--- resources/views/front/account/registration.blade.php
+++ resources/views/front/account/registration.blade.php
@@ -74,26 +74,90 @@
         });
 
         $.ajax({
-            type: 'POST',
+            type: 'post',
             url: "{{ route('account.processRegisteration') }}",
             data: $("#registerationForm").serializeArray(),
             dataType: 'json',
             success: function (response) {
-                const fields = ["name", "email", "password", "confirm_password"];
-
-                fields.forEach(field => {
-                    const errorField = $(`#${field}`);
-                    const feedbackField = errorField.siblings('p');
-
-                    if (response.status === false && response.errors[field]) {
-                        errorField.addClass('is-invalid');
-                        feedbackField.addClass('invalid-feedback').html(response.errors[field]);
+                if (response.status == false) {
+                    var errors = response.errors;
+                    if (errors.name) {
+                        $("#name")
+                            .addClass('is-invalid')
+                            .siblings('p')
+                            .addClass('invalid-feedback')
+                            .html(errors.name);
                     } else {
-                        errorField.removeClass('is-invalid');
-                        feedbackField.removeClass('invalid-feedback').html('');
-                        window.location.href='{{ route('account.login') }}'
+                        $("#name")
+                            .removeClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback')
+                            .html('');
                     }
-                });
+
+                    if (errors.email) {
+                        $("#email")
+                            .addClass('is-invalid')
+                            .siblings('p')
+                            .addClass('invalid-feedback')
+                            .html(errors.email);
+                    } else {
+                        $("#email")
+                            .removeClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback')
+                            .html('');
+                    }
+
+                    if (errors.password) {
+                        $("#password")
+                            .addClass('is-invalid')
+                            .siblings('p')
+                            .addClass('invalid-feedback')
+                            .html(errors.password);
+                    } else {
+                        $("#password")
+                            .removeClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback')
+                            .html('');
+                    }
+
+                    if (errors.confirm_password) {
+                        $("#confirm_password")
+                            .addClass('is-invalid')
+                            .siblings('p')
+                            .addClass('invalid-feedback')
+                            .html(errors.confirm_password);
+                    } else {
+                        $("#confirm_password")
+                            .removeClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback')
+                            .html('');
+                    }
+                }else{
+                    $("#name")
+                            .removeClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback')
+                            .html('');
+                    $("#email")
+                            .removeClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback')
+                            .html('');
+                    $("#password")
+                            .removeClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback')
+                            .html('');
+                    $("#confirm_password")
+                            .removeClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback')
+                            .html('');
+                }
             }
         });
     });
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[HomeController::class,'index'])->name('home');
-Route::get('/account/register',[AccountController::class,'registration'])->name('account.register');
-Route::get('/account/login',[AccountController::class,'login'])->name('account.login');
-Route::post('/account/processRegisteration',[AccountController::class,'processRegisteration'])->name('account.processRegisteration');
+
+
+Route::group(['account'],function(){
+    // guest routes
+    Route::group(['middleware'=>'guest'],function() {
+        Route::get('/account/register',[AccountController::class,'registration'])->name('account.register');
+        Route::post('/account/processRegisteration',[AccountController::class,'processRegisteration'])->name('account.processRegisteration');
+        Route::get('/account/login',[AccountController::class,'login'])->name('account.login');
+        Route::post('/account/authenticate',[AccountController::class,'authenticate'])->name('account.authenticate');
+    });
+
+    // authenticated routes
+    Route::group(['middleware'=>'auth'],function() {
+        Route::get('/account/profile',[AccountController::class,'profile'])->name('account.profile');
+        Route::get('/account/logout',[AccountController::class,'logout'])->name('account.logout');
+});
+});
